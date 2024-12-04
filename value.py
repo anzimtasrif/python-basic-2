b2e8698b885673c8122aa7c600d75af6202e41f8
Anzim Tasrif Athoi <109681624+anzimtasrif@users.noreply.github.com>
2024-12-04 23:46:51 +0600

    Add files via upload

---
+++ value.py
@@ -0,0 +1,37 @@
+#None type data
+x= None
+print(type(x))
+
+#List type Data
+li=['Anzim','Tasrif','Saif']
+print(li)
+
+#tuple type data
+ti=(12,28,32,48,50)
+print(ti)
+
+#range type data
+ran = range(6)
+for i in ran:
+    print(i)
+
+#Operators
+a = 20
+b = 80
+print(a+b)    
+#subtraction
+print(b-a)
+#Multiplication
+print(a*b)
+#Division
+print(a/b)
+#Modulus
+print(a%b)
+#Exponentiation
+x=5
+y=2
+print(x**y)
+#Floor Division
+x=15
+y=2
+print(x//y)
